6190d67729e741a142bb2656a0c94c128885f39b
Vikash Kumar <vikash0cv@gmail.com>
2023-07-05 01:08:10 +0530

    added DbConnect controller, Users model and changed sharito favicon

---
+++ src/controllers/DbConnect.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        mongoose.connect(process.env.MONGO_URI!);
+        const connection = mongoose.connection;
+        connection.on('connected', () => {
+            console.log('MongoDB connected successfully');
+        })
+
+        connection.on('error', (err) => {
+            console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
+            process.exit();
+        })
+
+    } catch (error) {
+        console.log(`Somegthing went wrong: ${error}`);
+        process.exit();
+    }
+}
